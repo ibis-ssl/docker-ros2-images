3dd7201e4547b3e85cba494a9daa5ab4647dbc94
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2020-06-27 22:43:08 +0900

    Add ros2 eloquent

---
+++ ros2-eloquent/docker_build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build . -t ibisssl/ros2-eloquent
---
+++ ros2-eloquent/docker_run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+image="ibisssl/ros2-eloquent"
+container="ros2-eloquent"
+
+if [ "$DOCKER_ROS_IP" == "" ]; then
+    export DOCKER_ROS_IP=localhost
+fi
+
+xhost +si:localuser:root
+
+# if ${container_name} exists
+docker rm ${container}
+
+EXE=${@:-"/bin/bash"}
+
+docker run --rm -it \
+    --privileged \
+    --gpus=all \
+    --net=host \
+    --env=DOCKER_ROS_IP \
+    --env=DOCKER_ROS_MASTER_URI \
+    --env=DOCKER_ROS_SETUP \
+    --env=DISPLAY \
+    --env=QT_X11_NO_MITSHM=1 \
+    --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw \
+    --name=${container} \
+    --volume=${PROG_DIR:-$(pwd)}:/userdir \
+    -w=/userdir \
+    ${image} ${EXE}
